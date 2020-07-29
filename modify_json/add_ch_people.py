0afeb18579ad2f5fbd65aca8fbbf6b499691e46b
tonycush <56894349+tonycush@users.noreply.github.com>
2020-07-29 20:05:29 +0100

    comp house - people data scraped

--- modify_json/add_ch_people.py
+++ modify_json/add_ch_people.py
@@ -6,23 +6,17 @@ import lxml
 import json
 import time
 
-def check_ixbrl(x):
-    if "iXBRL" in x:
-        return "Yes"
-    else:
-        return "No"
-
 # reading in the updated crunchbase file to get company numbers
 with open('my_files/crunchbase_info_tidy.json') as json_file:
     data = json.load(json_file)
 
 businesses = []
-for x in data[0:3]:
+for x in data:
     businesses.append(x["company_number"])
 
 order = 1
 for business in businesses:
-    business_ixbrl = []
+    
     time.sleep(1)
     print("\n\t***\n")
     filehistory = "https://beta.companieshouse.gov.uk/company/" + business + "/officers"
@@ -43,22 +37,61 @@ for business in businesses:
     total_officers = int(company_appointments_split[0].split(" ")[0])
     total_resignations = int(company_appointments_split[1].split(" ")[0])
 
-    print("** NOW THE APPOINTMENTS **")
-    company_appointments_list = soup.find(attrs = {"class":"appointments-list"})
+    company_officers = []
+    company_appointments_list = soup.find(attrs = {"class":"appointments-list"})    
+    appointments = company_appointments_list.findAll('div', recursive = False)
+    for appointment in appointments:
+        officer_details = appointment.findChildren(recursive = False)
+        officer_info = '{'
+        for o_detail in officer_details:
+            if o_detail.has_attr("class"):
+                if 'heading-medium' in o_detail.attrs['class']:
+                    o_key = "Officer"
+                    o_value = (o_detail.text.strip()) 
+                    #print(o_value)
+                    if "'" in o_value:
+                        o_value = o_value.replace("'","\'")
+                    o_linkage = o_detail.find("a").get("href")
+                    o_link_value = "https://beta.companieshouse.gov.uk"+o_linkage
+
+                    officer_info+= '"'+o_key+'":"'+o_value+'","Officer link":"'+o_link_value+'",' 
+                else:
+                    dls = o_detail.findAll('dl')
+                    for dl in dls:
+                        dtease = dl.findAll('dt')
+                        for dt in dtease:
+                            o_key = dt.text
+                            if "Role" in o_key[:4]:
+                                role_status = o_key[4:].strip()
+                                o_key = 'Status":"'+role_status+'","'+o_key[:4]
+                            o_value = dt.nextSibling.nextSibling.text.strip()
+                            officer_info+= '"'+o_key+'":"'+o_value+'",'  
+
+            else:
+                dts = o_detail.findAll('dt')
+                for dt in dts:
+                    o_key = dt.text
+                    o_value = dt.nextSibling.nextSibling.text.strip()
+                    #print(o_value)
+                    o_value = o_value.replace("'","\'")
+                    if '"' in o_value:
+                        o_value = o_value.replace('"','')
+                        print(o_value)
+                    officer_info+= '"'+o_key+'":"'+o_value+'",'  
+            
+        officer_info = officer_info[:-1]+"}"
+        #print(officer_info[150:170])
+        company_officers.append(json.loads(officer_info))
+
+    company_house_people_string = '{"num_officers":"'+str(total_officers)+'","num_resignation":"'+str(total_resignations)+'","officers":""}'  
+    ch_people_json = json.loads(company_house_people_string)
+    bus_index = (businesses.index(business))
+    if data[bus_index]['company_number'] ==business:
+        data[bus_index]['company_house_people']= ch_people_json
+        data[bus_index]['company_house_people']['officers'] = company_officers
     
-    print(type(company_appointments_list)) 
-    appointments = company_appointments_list.children
-    print(type(appointments)) 
-    #print(appointments.attrs) 
-    print(len(list(appointments)))
-    for app in appointments:
-        print(app)
+    order += 1   
 
-    order += 1
 
-    
-
-"""
 with open('my_files/crunchbase_info_tidy.json','w') as outfile:
     json.dump(data,outfile)
-"""
